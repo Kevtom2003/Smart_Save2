4e7b161e6cde73cbcb6e34593f457deffa9d683a
blakeaolson <blakeaolson1@gmail.com>
2023-01-29 00:49:28 -0600

    Added Pie Chart

--- client/src/scenes/budget/index.jsx
+++ client/src/scenes/budget/index.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import ReactApexChart from "react-apexcharts";
+import { useState } from 'react';
 
 const Budget = () => {
-    return <div>Budget</div>
+    const [state, setState] = useState({
+      series: [44, 55, 13, 33],
+      options: {
+        labels: ['Food', 'School', 'Leisure', 'Tuition'],
+        chart: {
+          width: 380,
+          type: 'donut',
+        },
+        dataLabels: {
+          enabled: false
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              show: false
+            }
+          }
+        }],
+        legend: {
+          position: 'right',
+          offsetY: 0,
+          height: 230,
+        }
+      },
+    })
+    return (
+      <div>
+        <ReactApexChart options={state.options} series={state.series} type="donut" width={330} />
+      </div>
+    );
 
 };
 
