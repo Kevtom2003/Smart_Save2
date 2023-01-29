ceb8558bbcd6c9552251ef6830aac5d8a6b84eaf
blakeaolson <blakeaolson1@gmail.com>
2023-01-29 00:39:01 -0600

    Added investments page

--- client/src/scenes/analytics/index.jsx
+++ client/src/scenes/analytics/index.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
 import Graph from "../../components/Graph";
+import { useState } from 'react';
+import dataVals from '../dashboard/data.js';
+import ReactApexChart from "react-apexcharts";
+import { Box } from '@mui/system';
 
 const Analytics = () => {
-    return (    
-        <div className="container">
-          This is the analytics
-        </div>
+  const [state, setState] = useState({
+    series: [{
+      name: 'SPDR S&P 500 ETF Trust (SPY)',
+      data: dataVals
+    }],
+    options: {
+      chart: {
+        type: 'area',
+        stacked: false,
+        height: 350,
+        zoom: {
+          type: 'x',
+          enabled: true,
+          autoScaleYaxis: true
+        },
+        toolbar: {
+          autoSelected: 'zoom'
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      markers: {
+        size: 0,
+      },
+      title: {
+        text: 'SPDR S&P 500 ETF Trust (SPY)',
+        align: 'left'
+      },
+      
+      yaxis: {
+        labels: {
+          formatter: function (val) {
+            return (val / 1000000).toFixed(0);
+          },
+        },
+        title: {
+          text: 'Price'
+        },
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+      tooltip: {
+        shared: false,
+        y: {
+          formatter: function (val) {
+            return (val / 1000000).toFixed(0)
+          }
+        }
+      }
+    },
+  })
+    return (  
+      <div>
+        <ReactApexChart options={state.options} series={state.series} type="line" height={350}/>
+        <Box textAlign={'center'}>ML Based Stock Predictions</Box>
+      </div>
     );
 
 };
