bd7d9cc76e9aa55ac0087553de6406280e306ec3
blakeaolson <blakeaolson1@gmail.com>
2023-01-29 12:23:50 -0600

    Made chart responsive

--- client/src/scenes/analytics/index.jsx
+++ client/src/scenes/analytics/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Graph from "../../components/Graph";
 import { useState } from 'react';
-import dataVals from '../dashboard/data.js';
+import primaryData from '../dashboard/data.js'
+import dataVals from './data.js';
 import ReactApexChart from "react-apexcharts";
 import { Box } from '@mui/system';
+import { Button } from '@mui/material';
 
 const Analytics = () => {
   const [state, setState] = useState({
     series: [{
       name: 'SPDR S&P 500 ETF Trust (SPY)',
-      data: dataVals
+      data: primaryData
     }],
     options: {
       chart: {
@@ -63,10 +65,70 @@ const Analytics = () => {
       }
     },
   })
+  function buttonClicked(){
+    setState({
+      series: [{
+        name: 'SPDR S&P 500 ETF Trust (SPY)',
+        data: dataVals
+      }],
+      options: {
+        chart: {
+          type: 'area',
+          stacked: false,
+          height: 350,
+          zoom: {
+            type: 'x',
+            enabled: true,
+            autoScaleYaxis: true
+          },
+          toolbar: {
+            autoSelected: 'zoom'
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        markers: {
+          size: 0,
+        },
+        title: {
+          text: 'SPDR S&P 500 ETF Trust (SPY)',
+          align: 'left'
+        },
+        
+        yaxis: {
+          labels: {
+            formatter: function (val) {
+              return (val / 1000000).toFixed(0);
+            },
+          },
+          title: {
+            text: 'Price'
+          },
+        },
+        xaxis: {
+          show: false,
+          labels: {
+            show: false
+          },
+          type: 'datetime'
+        },
+        tooltip: {
+          shared: false,
+          y: {
+            formatter: function (val) {
+              return (val / 1000000).toFixed(0)
+            }
+          }
+        }
+      },
+    })
+  }
     return (  
       <div>
         <ReactApexChart options={state.options} series={state.series} type="line" height={350}/>
-        <Box textAlign={'center'}></Box>
+        <Box textAlign={'center'}>Enter Smart Score</Box>
+        <Button onClick={buttonClicked}>Bad Smart Score</Button>
       </div>
     );
 
